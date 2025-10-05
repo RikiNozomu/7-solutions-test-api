73e35b44817445f6880ccb73c12d2e401afa5cad
Riki Nozomu <rikinozomu2022@gmail.com>
2025-10-05 15:40:59 +0700

    added client and readme

---
+++ README.md
@@ -0,0 +1,27 @@
+# Testing project for 7 Solution Front-End testing Part 2
+
+This is a project for [`7 Solution Front-End testing`](https://github.com/7-solutions/frontend-assignment).
+
+## Getting Started
+
+First, run the server:
+
+```bash
+npm run start:server
+```
+
+Then, run the client in another windows:
+
+```bash
+npm run start:client
+```
+
+Or, connect to the server via gRPC postman like this:
+
+## Testing
+```bash
+npm run test
+```
+
+## Production on Vercel
+Open [https://7-solutions-test-flax.vercel.app/](https://7-solutions-test-flax.vercel.app/) with your browser to see the result.
---
+++ client.ts
@@ -0,0 +1,36 @@
+import * as grpc from "@grpc/grpc-js";
+import * as protoLoader from "@grpc/proto-loader";
+import type { ProtoGrpcType } from "./proto/department.ts";
+import type { Response as ResponseData } from "./proto/department/Response.ts";
+import type { Empty } from "./proto/google/protobuf/Empty.ts";
+
+const host = "0.0.0.0:9090";
+const packageDefinition = protoLoader.loadSync("./proto/department.proto");
+const proto = grpc.loadPackageDefinition(
+  packageDefinition
+) as unknown as ProtoGrpcType;
+
+const client = new proto.department.Departments(
+  host,
+  grpc.credentials.createInsecure()
+);
+
+const deadline = new Date();
+deadline.setSeconds(deadline.getSeconds() + 5);
+client.waitForReady(deadline, (error?: Error) => {
+  if (error) {
+    console.log(`Client connect error: ${error.message}`);
+  } else {
+    client.GetAll(
+      undefined as unknown as Empty,
+      (error?: grpc.ServiceError | null, rep?: ResponseData) => {
+        if (error) {
+          console.error(error.message);
+        } else if (rep) {
+          console.log(`(client) Got server message:`);
+          console.log(JSON.stringify(rep));
+        }
+      }
+    );
+  }
+});
